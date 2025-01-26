eadac995211ac3b0459d2a6b3bc184b9eb0be9d2
ash <chxmeleonweng@gmail.com>
2025-01-26 19:56:55 +0800

    feat: init the project

---
+++ lua/kimuku/pattern/ki.lua
@@ -0,0 +1,35 @@
+return {
+  base = {
+    bg = "",
+    float = "",
+    sel = "",
+    ui = "",
+    com = "",
+    fg = "",
+  },
+  dark = {
+
+    red = "",
+    yellow = "",
+    green = "",
+    cyan = "",
+    blue = "",
+    magenta = "",
+  },
+  middle = {
+    red = "",
+    yellow = "",
+    green = "",
+    cyan = "",
+    blue = "",
+    magenta = "",
+  },
+  bright = {
+    red = "",
+    yellow = "",
+    green = "",
+    cyan = "",
+    blue = "",
+    magenta = "",
+  },
+}
